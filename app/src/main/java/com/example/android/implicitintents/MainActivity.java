c8c21e97d901e80b7bed8054c1a92ee9c70e7c06
Eduardo Przysiezny <eduardo_thp@yahoo.com.br>
2018-01-21 16:32:52 -0800

    Exercise completion

--- app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -48,12 +48,14 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickOpenAddressButton(View v) {
-        // TODO (5) Store an address in a String
+        // (5) Store an address in a String
+        String address = "geo:0,0?q=5108 Penn Av S, Minneapolis, MN";
 
-        // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+        // (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+        Uri mapUri = Uri.parse(address);
 
-        // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        // (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
+        showMap(mapUri);
     }
 
     /**
@@ -112,13 +114,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // TODO (1) Create a method called showMap with a Uri as the single parameter
-    // Do steps 2 - 4 within the showMap method
-        // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
+    // (1) Create a method called showMap with a Uri as the single parameter
+    void showMap(Uri mapUri) {
+        // Do steps 2 - 4 within the showMap method
+        // (2) Create an Intent with action type, Intent.ACTION_VIEW
+        // (3) Set the data of the Intent to the Uri passed into this method
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
 
-        // TODO (3) Set the data of the Intent to the Uri passed into this method
-
-        // TODO (4) Verify that this Intent can be launched and then call startActivity
+        // (4) Verify that this Intent can be launched and then call startActivity
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
 
 
 }
